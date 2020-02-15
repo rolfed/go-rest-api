54e54e34cdda2d375082cf433c66544af590efd0
Danniel Rolfe <talkto@dannielrolfe.com>
2020-02-14 18:02:17 -0800

    Add initial file structure

--- src/main.go
+++ src/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will
+	// signal through ctx.Done() that the request has timed out and
+	// further processing should be stopped
+	router.Use(middleware.Timeout(60 * time.Second))
+
+	router.Route("/v1", func(r chi.Router) {
+		// Routes
+	})
+
+	http.ListenAndServe(":8001", router)
 }
